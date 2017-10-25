3af6dcd0697a6b15f4372d258df1085ccb5013a8
Jesse Chan <jc@lineageos.org>
2017-10-24 21:45:20 -0700

    hero: initial recovery support
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.samsungexynos8890
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := twrp.fstab
+LOCAL_MODULE_TAGS  := debug eng
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_SRC_FILES    := recovery/etc/twrp.fstab
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
+include $(BUILD_PREBUILT)
--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,7 @@
 # Copyright (C) 2017 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 # Device specific property overrides
+
+# Boot device
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+  ro.boot.bootdevice=155a0000.ufs
