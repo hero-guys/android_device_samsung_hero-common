674b739f240f6e3b0ec5dfeb899ba363894f5a56
ivanmeler <i_ivan@windowslive.com>
2017-11-25 12:23:52 +0100

    Add SECCameraProperties include

--- camera/Android.mk
+++ camera/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES += \
     system/core/include \
     system/media/camera/include \
-	frameworks/native/libs/arect/include
+    frameworks/native/libs/arect/include
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 
