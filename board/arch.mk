68187e2d3c704a81050d514798eb645f2b6a648f
deedwar <deed821@gmail.com>
2017-12-23 04:33:52 +1100

    set correct primary target CPU there was issues with 32-bit applications on nougat aosp but let's give it a try now

--- board/arch.mk
+++ board/arch.mk
@@ -2,8 +2,8 @@
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
 
 # Secondary Architecture
 TARGET_2ND_ARCH := arm
